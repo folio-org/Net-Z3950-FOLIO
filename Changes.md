d8f2f8ce3ee8a57f23f52be1d0789d65b634f364
Mike Taylor <mike@indexdata.com>
2018-12-06 13:13:59 +0100

    New

---
+++ Changes.md
@@ -0,0 +1,5 @@
+# Revision history for Perl extension Net::Z3950::FOLIO.
+
+## 0.01  Thu Dec  6 13:03:26 2018
+* Original version; created by `h2xs -X --name=Net::Z3950::FOLIO --compat-version=5.8.0 --omit-constant --skip-exporter --skip-ppport`
+
---
+++ lib/Net/Z3950/FOLIO.pm
@@ -0,0 +1,39 @@
+package Net::Z3950::FOLIO;
+
+use 5.008000;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+
+=head1 NAME
+
+Net::Z3950::FOLIO - Z39.50 server for FOLIO bibliographic data
+
+=head1 SYNOPSIS
+
+  use Net::Z3950::FOLIO;
+  XXX explain
+
+=head1 DESCRIPTION
+
+XXX explain
+
+=head1 SEE ALSO
+
+Net::Z3950::SimpleServer
+
+=head1 AUTHOR
+
+Mike Taylor, E<lt>mike@indexdata.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2018 by Index Data
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the Apache Licence 2.0: see the LICENSE file.
+
