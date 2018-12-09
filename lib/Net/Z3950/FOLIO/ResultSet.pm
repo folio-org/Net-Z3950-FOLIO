d436c7c8918e1c85a670efbd82643cb0ad8507e6
Mike Taylor <mike@indexdata.com>
2018-12-09 23:36:39 +0000

    Provide records API

--- lib/Net/Z3950/FOLIO/ResultSet.pm
+++ lib/Net/Z3950/FOLIO/ResultSet.pm
@@ -12,9 +12,25 @@ sub new {
     }, $class;
 }
 
+sub insert_records {
+    my $this = shift();
+    my($offset, $records) = @_;
+
+    for (my $i = 0; $i < @$records; $i++) {
+	$this->{instances}->[$offset + $i] = $records->[$i];
+    }
+}
+
 sub totalRecords {
     my $this = shift();
     return $this->{totalRecords};
+}
+
+sub record {
+    my $this = shift();
+    my($index1) = @_;
+
+    return $this->{instances}->[$index1-0];
 }
 
 1;
