63ab98d45422d134498abb655470f625fd6a126d
Mike Taylor <mike@indexdata.com>
2020-08-18 17:34:13 +0100

    Check MARC file exists

--- doc/load-marc-data-into-folio.sh
+++ doc/load-marc-data-into-folio.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Invoke as: ./load-marc-data-into-folio.sh sample100.marc
+
 if [ $# -ne 1 ]; then
    echo "Usage: $0 <MARCfile>" >&2
    exit 1
 fi
 filename="$1"
+if [ ! -f "$filename" ]; then
+    echo "$0: no such MARC file: $filename" >&2
+    exit 2
+fi
 
 . ~/.okapi
 tmpfile1=`mktemp`
@@ -34,7 +40,7 @@ curl --silent --location --request POST "$OKAPI_URL/data-import/uploadDefinition
 		> $tmpfile2
 
 echo "=== Stage 3 ==="
-curl --silent --location --request POST "$OKAPI_URL/data-import/uploadDefinitions/$uploadDefinitionId/processFiles?defaultMapping=true" \
+curl -i --silent --location --request POST "$OKAPI_URL/data-import/uploadDefinitions/$uploadDefinitionId/processFiles?defaultMapping=true" \
 	--header "Content-Type: application/json" \
 	--header "X-Okapi-Tenant: $OKAPI_TENANT" \
 	--header "X-Okapi-Token: $OKAPI_TOKEN" \
