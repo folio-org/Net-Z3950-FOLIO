89cfe9ab87fb9873cffcf2731e3bc05ca87c4ec7
Mike Taylor <mike@indexdata.com>
2021-02-23 17:55:13 +0000

    Invert marcHoldings configuration

--- lib/Net/Z3950/FOLIO/HoldingsInfo.pm
+++ lib/Net/Z3950/FOLIO/HoldingsInfo.pm
@@ -12,26 +12,41 @@ sub insertHoldingsInfo {
     my $holdingsObjects = Net::Z3950::FOLIO::OPACXMLRecord::_makeHoldingsRecords($ihi->{holdingsRecords2}, $marc);
 
     for (my $i = 0; $i < @$holdingsObjects; $i++) {
-	my $holdingsObject = $holdingsObjects->[$i];
+	my $holdingsMap = _listOfPairs2map($holdingsObjects->[$i]);
 	my $marcField; # Annoyingly, this can't be created with no subfields
 
-	for (my $j = 0; $j < @$holdingsObject; $j++) {
-	    my $keyVal = $holdingsObject->[$j];
-	    my($key, $val) = @$keyVal;
+	my $elements = $marcCfg->{elements};
+	foreach my $subfield (sort keys %$elements) {
+	    next if $subfield =~ /^#/;
+	    my $name = $elements->{$subfield};
+	    my $val = $holdingsMap->{$name};
+	    # warn "considering key '$subfield' mapped to '$name' with value '$val'";
+	    next if !$val;
 
-	    my $target = $marcCfg->{elements}->{$key};
-	    if ($target) {
-		if ($marcField) {
-		    $marcField->add_subfields($target, $val);
-		} else {
-		    # Delayed creation
-		    $marcField = MARC::Field->new($marcCfg->{field}, @{ $marcCfg->{indicators}}, $target, $val);
-		}
+	    if ($marcField) {
+		$marcField->add_subfields($subfield, $val);
+	    } else {
+		# Delayed creation
+		$marcField = MARC::Field->new($marcCfg->{field}, @{ $marcCfg->{indicators}}, $subfield, $val);
 	    }
 	}
 
 	$marc->append_fields($marcField) if $marcField;
     }
+}
+
+
+sub _listOfPairs2map {
+    my($listOfPairs) = @_;
+
+    my $map;
+    for (my $j = 0; $j < @$listOfPairs; $j++) {
+	my $keyVal = $listOfPairs->[$j];
+	my($key, $val) = @$keyVal;
+	$map->{$key} = $val;
+    }
+
+    return $map;
 }
 
 
