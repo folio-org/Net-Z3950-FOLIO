059bc19e9644598c6fe07218973fc4455669a546
Mike Taylor <mike@miketaylor.org.uk>
2022-06-28 12:16:00 +0100

    Substitute ${artifactId} as well as ${version}

--- descriptors/transform-descriptor.pl
+++ descriptors/transform-descriptor.pl
@@ -1,2 +1,2 @@
 version=`perl -I lib -MNet::Z3950::FOLIO -e '$x = $Net::Z3950::FOLIO::VERSION; $x =~ s/.//; print $x'`
-cat ${@+"$@"} | perl -npe 's/\${version}/'"$version"'/g'
+cat ${@+"$@"} | perl -npe 's/\${version}/'"$version"'/g; s/\${artifactId}/mod-z3950/g;'
