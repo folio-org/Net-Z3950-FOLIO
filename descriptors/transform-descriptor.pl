ddc3ba8ca42046acb8f26d5779b09c625dc69d96
Mike Taylor <mike@miketaylor.org.uk>
2022-06-28 11:45:10 +0100

    Working version of transform-descriptor.pl

--- descriptors/transform-descriptor.pl
+++ descriptors/transform-descriptor.pl
@@ -1,3 +1,2 @@
-perl -I lib -MNet::Z3950::FOLIO -e 'print $Net::z3950::FOLIO::VERSION'
-
-exec cat ${@+"$@"}
+version=`perl -I lib -MNet::Z3950::FOLIO -e '$x = $Net::Z3950::FOLIO::VERSION; $x =~ s/.//; print $x'`
+cat ${@+"$@"} | perl -npe 's/\${version}/'"$version"'/g'
