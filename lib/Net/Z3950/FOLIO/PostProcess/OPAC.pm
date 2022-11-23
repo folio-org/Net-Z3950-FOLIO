70378a9193ff4a2b749c5b0e3f9c3b0ecdc28d5c
Mike Taylor <mike@indexdata.com>
2022-11-23 17:14:12 +0000

    Implement OPACrecod post-processing

--- lib/Net/Z3950/FOLIO/PostProcess/OPAC.pm
+++ lib/Net/Z3950/FOLIO/PostProcess/OPAC.pm
@@ -4,16 +4,52 @@ use strict;
 use warnings;
 use utf8;
 
+use Data::Dumper; $Data::Dumper::INDENT = 2;
 use Net::Z3950::FOLIO::PostProcess::Transform qw(transform);
 
 
 sub postProcessHoldings {
     my($cfg, $holdings) = @_;
 
-    # use Data::Dumper; $Data::Dumper::INDENT = 2; print Dumper($cfg);
-    # XXX do it!
+    foreach my $holding (@$holdings) {
+	processListOfFields($cfg, $holding, 0);
+    }
 
     return $holdings;
+}
+
+
+sub processListOfFields {
+    my($cfg, $list, $isItem) = @_;
+    my $thisCfg = $cfg->{$isItem ? 'circulation' : 'holding' };
+
+    my $getFieldFromRecord = sub {
+	my($fieldname) = @_;
+
+	# XXX maybe we should make a hash the first time we need to do this
+	foreach (my $i = 0; $i < @$list; $i++) {
+	    my $field = $list->[$i];
+	    my($name, $value) = @$field;
+	    if ($name eq $fieldname) {
+		return $value;
+	    }
+	}
+
+	return undef;
+    };
+
+    foreach (my $i = 0; $i < @$list; $i++) {
+	my $field = $list->[$i];
+	my($name, $value) = @$field;
+	if (!$isItem && $name eq 'circulations') {
+	    processListOfFields($cfg, $value->[0], 1);
+	} else {
+	    my $rule = $thisCfg->{$name};
+	    if ($rule) {
+		$field->[1] = transform($rule, $value, $getFieldFromRecord);
+	    }
+	}
+    }
 }
 
 
