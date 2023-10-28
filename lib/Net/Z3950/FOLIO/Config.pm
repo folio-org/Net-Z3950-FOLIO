eae11804f8f100e98002469a024126e82d7ddf66
Mike Taylor <mike@indexdata.com>
2023-10-28 19:45:23 +0100

    Documentation for `fieldDefinitions`

--- lib/Net/Z3950/FOLIO/Config.pm
+++ lib/Net/Z3950/FOLIO/Config.pm
@@ -207,6 +207,11 @@ Net::Z3950::FOLIO::Config - configuration file for the FOLIO Z39.50 gateway
     "queryFilter": "source=marc",
     "graphqlQuery": "instances.graphql-query",
     "chunkSize": 5,
+    "fieldDefinitions": {
+      "circulation": {
+	"availableThru": "permanentLoanType"
+      }
+    },
     "marcHoldings": {
       "restrictToItem": 0,
       "field": "952",
@@ -240,7 +245,7 @@ Net::Z3950::FOLIO::Config - configuration file for the FOLIO Z39.50 gateway
 	    "pattern": "(.*)",
 	    "replacement": "%{_callNumberPrefix}$1%{_callNumberSuffix}"
 	  }
-	},
+	}
       }
     }
   }
@@ -396,6 +401,34 @@ search. This can be tweaked to tune performance. Setting it too low
 will result in many requests with small numbers of records returned
 each time; setting it too high will result in fetching and decoding
 more records than are actually wanted.
+
+=head2 C<fieldDefinitions>
+
+An optional object specifying how the sources from which some
+particular holdings fields should draw their data, overriding the
+default sources.
+
+The keys of this object specify domains in which the fields can be
+found: supported domains are C<holding> and C<circulation>}.
+
+Within the C<holdings> domain, no fields are presently supported;
+within the C<circulation> domain, only the C<availableThru> field is
+presently supported. Support for further fields may be added as
+required.
+
+Each field within a domain is specified by its name as key, and the
+corresponding value is a string specifying a path from which to fetch
+the data. These strings follow the same syntax as L<the get function
+in JavaScript's lodash library|https://docs-lodash.com/v4/get/>. It is
+a dot-separate sequence of source field-names to navigate down, with
+bracketed numbers indicating the selection of one element of an
+array. For example, the path
+C<temporaryLocation.servicePoints[0].discoveryDisplayName> fetches the
+C<discoveryDisplayName> field from the first element of the
+C<servicePoints> array within the C<temporaryLocation> structure. It
+will work when running on data with the shape C<{ temporaryLocation: {
+servicePoints: [ { discoveryDisplayName: "Main" } ] } }>.
+
 
 =head2 C<marcHoldings>
 
